23544c1cebd8d89f8fbc38f94111c9a14e22eb0d
A. Wayne Roberts <arob932@wgu.edu>
2024-06-20 16:02:57 -0500

    Add CommandLineInterface class to ui

---
+++ ui/command_line_interface.py
@@ -0,0 +1,15 @@
+class CommandLineInterface:
+    def __init__(self, delivery_system):
+        self.delivery_system = delivery_system
+
+    def run(self):
+        pass
+
+    def display_package_status(self, package_id):
+        pass
+
+    def display_all_packages_status(self):
+        pass
+
+    def display_total_mileage(self):
+        pass
