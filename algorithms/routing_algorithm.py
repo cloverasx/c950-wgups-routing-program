3fbbddfed73fce9e071524a6fb0f80cb73f67bc3
A. Wayne Roberts <arob932@wgu.edu>
2024-06-20 15:58:27 -0500

    Add Truck class to algorithms

--- algorithms/routing_algorithm.py
+++ algorithms/routing_algorithm.py
@@ -8,3 +8,17 @@ class RoutingAlgorithm:
 
     def calculate_total_distance(self):
         pass
+    
+
+class Truck:
+    def __init__(self, capacity, speed):
+        self.capacity = capacity
+        self.speed = speed
+        self.packages = []
+        self.current_location = "HUB"
+
+    def load_package(self, package):
+        self.packages.append(package)
+
+    def deliver_packages(self):
+        pass
