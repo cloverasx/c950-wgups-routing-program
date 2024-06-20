d0aff49536dc65805d5ed9f93ae9d6bfb5a22ee9
A. Wayne Roberts <arob932@wgu.edu>
2024-06-20 15:55:57 -0500

    Add RoutingAlgorithm class to algorithms

---
+++ algorithms/routing_algorithm.py
@@ -0,0 +1,10 @@
+class RoutingAlgorithm:
+    def __init__(self, packages, distance_table):
+        self.packages = packages
+        self.distance_table = distance_table
+
+    def optimize_route(self):
+        pass
+
+    def calculate_total_distance(self):
+        pass
