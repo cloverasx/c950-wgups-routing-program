f038668573a90701aaea86e1bc416f26427e1a2e
wayner <cloverasx@gmail.com>
2024-07-15 11:42:10 -0500

    Refactor NearestNeighbor with RoutingAlgorithm

--- algorithms/routing_algorithm.py
+++ algorithms/routing_algorithm.py
@@ -3,22 +3,7 @@ from collections import deque
 
 
 class RoutingAlgorithm(ABC):
-    def __init__(self, distance_graph, packages):
-        self.distance_table = distance_graph
-        self.packages = packages
-
-    @abstractmethod
-    def route(self):
-        pass
-
-    @abstractmethod
-    def get_total_distance(self):
-        pass
-
-
-class NearestNeighbor(RoutingAlgorithm):
     def __init__(self, graph, packages, hub=None):
-        super().__init__(graph, packages)
         self.graph = graph
         self.packages = packages
         self.hub = hub or "4001 south 700 east"
@@ -26,28 +11,12 @@ class NearestNeighbor(RoutingAlgorithm):
         self.route_dict = {}
         self.total_distance = 0
 
+    @abstractmethod
     def route(self):
-        undelivered = deque(self.packages.get_all_ids())
-        current_location = self.hub
+        pass
 
-        while undelivered:
-            nearest_package_id = self._find_nearest_package(
-                current_location, undelivered
-            )
-            self._update_route(nearest_package_id, current_location)
-            current_location = self.packages.lookup(nearest_package_id).address
-            undelivered.remove(nearest_package_id)
-
-        self._return_to_hub(current_location)
-        return self.route_list, self.route_dict
-
-    def _find_nearest_package(self, current_location, undelivered):
-        return min(
-            undelivered,
-            key=lambda pid: self.graph.get_distance(
-                current_location, self.packages.lookup(pid).address
-            ),
-        )
+    def get_total_distance(self):
+        return self.total_distance
 
     def _update_route(self, package_id, current_location):
         package = self.packages.lookup(package_id)
@@ -76,8 +45,30 @@ class NearestNeighbor(RoutingAlgorithm):
         self.total_distance += distance_to_hub
         self.route_list.append(("Return", self.hub))
 
-    def get_total_distance(self):
-        return self.total_distance
+
+class NearestNeighbor(RoutingAlgorithm):
+    def route(self):
+        undelivered = deque(self.packages.get_all_ids())
+        current_location = self.hub
+
+        while undelivered:
+            nearest_package_id = self._find_nearest_package(
+                current_location, undelivered
+            )
+            self._update_route(nearest_package_id, current_location)
+            current_location = self.packages.lookup(nearest_package_id).address
+            undelivered.remove(nearest_package_id)
+
+        self._return_to_hub(current_location)
+        return self.route_list, self.route_dict
+
+    def _find_nearest_package(self, current_location, undelivered):
+        return min(
+            undelivered,
+            key=lambda pid: self.graph.get_distance(
+                current_location, self.packages.lookup(pid).address
+            ),
+        )
 
 
 class GreedyAlgorithm(RoutingAlgorithm):
